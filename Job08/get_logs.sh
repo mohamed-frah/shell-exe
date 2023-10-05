8faaab40ee5f48d001b2173c6238938ccb3116db
mohamed-frah <146708383+mohamed-frah@users.noreply.github.com>
2023-10-05 10:31:20 +0200

    Update get_logs.sh

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,5 +1,7 @@
 last basset | wc -l  > number_connection-`date +%d-%m-%Y-%H:%M`.txt
 tar -cvf "number_connection-`date +%d-%m-%Y-%H-%M`.tar" "number_connection-`date +%d-%m-%Y-%H:%M`.txt"
 mv "number_connection-`date +%d-%m-%Y-%H-%M`.tar" /home/basset/shell-exe/Job08/Backup
+#*/30 * * * * /home/basset/shell-exe/Job08/./get_logs.sh
+
 
 
